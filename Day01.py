e6cb5b47ee1f3d9794316a0e81a0cdab1d44f28d
Furqan1521 <169130271+Furqan1521@users.noreply.github.com>
2025-06-28 14:40:33 +0800

    Add files via upload

---
+++ Day01.py
@@ -0,0 +1,7 @@
+integer_input = int(input("Enter an integer: "))
+float_input = float(input("Enter a float: "))
+string_input = input("Enter a string: ")
+
+print(f"Integer value: {integer_input}")
+print(f"Float value: {float_input}")
+print(f"String value: {string_input}")
